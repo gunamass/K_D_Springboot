86a6c11fc316fb9fe98c8d653787366796696b57
GunaBocxy <163397395+GunaBocxy@users.noreply.github.com>
2025-10-28 17:14:46 +0530

    o

---
+++ src/test/java/com/bocxy/accountsAndFinance/KissTechCRMTest.java
@@ -0,0 +1,15 @@
+package com.bocxy.accountsAndFinance;
+
+import com.KissTech.crm.KissTechCRM;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest(classes = KissTechCRM.class)
+ public class KissTechCRMTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
